192d110d3f13bcfe2a5d292b4e2290c0a83c7d2f
AnastasiaMorozovaBPI205 <71143459+AnastasiaMorozovaBPI205@users.noreply.github.com>
2021-09-16 20:31:02 +0300

    Add files via upload

---
+++ avmorozova_6PW1/avmorozova_6PW1/ViewController.swift
@@ -0,0 +1,59 @@
+// ViewController.swift
+//  avmorozova_6PW1
+//
+//  Created by Anastasia on 12.09.2021.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var views: [UIView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fillColorSet()
+        roundCornersSetColor()
+    }
+    
+    
+    var colorSet = Set<UIColor>()
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        fillColorSet()
+        roundCornersSetColor()
+        
+        let button = sender as? UIButton
+        button?.isEnabled = false
+        
+        UIView.animate(withDuration: 2, animations: {
+            self.roundCornersSetColor()
+        }) { completion in
+            button?.isEnabled = true
+        }
+    }
+    
+    func fillColorSet()
+    {
+        while colorSet.count < views.count {
+            colorSet.insert(
+                UIColor(
+                    red: .random(in: 0...1),
+                    green: .random(in: 0...1),
+                    blue: .random(in: 0...1),
+                    alpha: 1
+                )
+            )
+        }
+    }
+    
+    func roundCornersSetColor()
+    {
+        for view in self.views {
+            view.layer.cornerRadius = 10
+            view.backgroundColor = self.colorSet.popFirst()
+        }
+    }
+    
+}
+
